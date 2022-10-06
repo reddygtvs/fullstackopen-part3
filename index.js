5fdade35a86951c444a28516854b481cee6e3521
Tushar Reddy <tushar@Tushars-MacBook-Air.local>
2022-10-07 00:52:33 +0530

    part 3.2

--- index.js
+++ index.js
@@ -28,6 +28,16 @@ app.get("/", (request, response) => {
   response.send("<h1>Welcome to the PhoneBook API<h1>");
 });
 
+app.get("/api/info", (request, response) => {
+  const fetchTime = new Date();
+  const size = Object.keys(persons).length;
+  console.log(fetchTime);
+  console.log(size);
+  response.send(`<h2>Phonebook has info for ${size} people</h2>
+  <h2>${fetchTime}`);
+  //response.json(`<h3></h3>`)
+});
+
 app.get("/api/persons", (request, response) => {
   response.json(persons);
 });
